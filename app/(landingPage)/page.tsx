712a744dc3beece90ac4b5cc07b519139a65a492
Joy Sutradhar <joysd.1005@fec.edu.bd>
2025-12-24 04:27:08 +0600

    feat: implement Lender Dashboard page and add animation component for enhanced user experience

--- app/(landingPage)/page.tsx
+++ app/(landingPage)/page.tsx
@@ -9,35 +9,45 @@ import Description from '@/components/Description';
 import Navbar from './_components/Navbar';
 import Features from './_components/Features';
 import Carousel from './_components/Carousel';
+import ComponentAnimation from './_components/Animation';
 
 const page = () => {
     return (
-        <div>
-            <Navbar />
-            <Banner />
-            <Features />
-            <div className='text-center'>
-                <Headline text="How It Works" />
-            </div>
-            <Description text="Simple 3-step process to transform your CRE financing" />
-            <HowItWorksOne />
-            <HowItWorksTwo />
-            <Carousel />
-
-            <div className='my-16'>
-                <div className='text-center'>
-                    <Headline text="Join the BANCre Capital Markets Network" />
-                </div>
-                <Description text="Become a BANCre broker and leverage technology to build your own capital markets 
-                origination business." />
-                <div className='flex items-center justify-center'>
-                    <BlackButtonRightIcon buttonName='Contact Us' iconName={FaArrowRight} width="w-[200px]" rounded="rounded-lg" />
-                </div>
-            </div>
-            <div className='mt-12'>
-                <Footer />
-            </div>
+      <div>
+        <Navbar />
+        <Banner />
+        <ComponentAnimation />
+        <Features />
+        <div className="text-center">
+          <Headline text="How It Works" />
         </div>
+        <Description text="Simple 3-step process to transform your CRE financing" />
+        <HowItWorksOne />
+        <HowItWorksTwo />
+        <Carousel />
+
+        <div className="my-16">
+          <div className="text-center">
+            <Headline text="Join the BANCre Capital Markets Network" />
+          </div>
+          <Description
+            text="Become a BANCre broker and leverage technology to build your own capital markets 
+                origination business."
+          />
+          <div className="flex items-center justify-center">
+            <BlackButtonRightIcon
+              buttonName="Contact Us"
+              iconName={FaArrowRight}
+              width="w-[200px]"
+              rounded="rounded-lg"
+            />
+          </div>
+        </div>
+
+        <div className="mt-12">
+          <Footer />
+        </div>
+      </div>
     );
 };
 
