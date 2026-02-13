b8e90366546e9a548026ed625791bd8dd45f39c8
Md Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2026-02-13 23:50:59 +0600

    fix issue from the not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Image from "next/image";
 import Link from "next/link";
 
 export const metadata: Metadata = {
@@ -9,6 +10,9 @@ export const metadata: Metadata = {
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center p-5">
+      <Link href="/">
+        <Image src={"/logo/BANCre.png"} width={200} height={200} alt="logo" className="mb-5" />
+      </Link>
       <h1 className="text-4xl font-bold mb-4"><span className="text-red-700">404</span> - Page Not Found</h1>
       <p className="text-lg text-gray-600 mb-6">
         The page you are looking for does not exist.
